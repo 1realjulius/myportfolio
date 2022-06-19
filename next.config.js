eae1696e159044656706df7ef6148834c91bbefe
1realjulius <iamjuliuseghan@gmail.com>
2022-06-19 23:48:39 +0000

    100

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ module.exports = {
   images: {
     loader: 'akamai',
     path: '',
+    trailingSlash: true,
+    reactStrictMode: true,
     assetPrefix: './',
+
   },
   reactStrictMode: true,
 }
