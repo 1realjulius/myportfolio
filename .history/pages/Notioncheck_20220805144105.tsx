8196052c12cb687fa720287c52e93c7426e491c2
1realjulius <iamjuliuseghan@gmail.com>
2022-08-05 14:42:03 +0000

    Light touches

--- .history/pages/Notioncheck_20220805144105.tsx
+++ .history/pages/Notioncheck_20220805144105.tsx
@@ -242,7 +242,7 @@ export default function Notioncheck() {
             <p className="mt-2 items-center text-center text-sm leading-6 text-slate-500">
               © 2022 Julius Eghan. All rights reserved.
             </p>
-            <div className="mt-4 flex items-center justify-center space-x-4 text-sm  leading-6 text-slate-700">
+            <div className="mt-4 flex items-center justify-center space-x-4 text-sm font-semibold leading-6 text-slate-700">
               <a href="/privacy-policy">Privacy policy</a>
               <div className="h-4 w-px bg-slate-500/20"></div>
               <a href="/changelog">Changelog</a>
