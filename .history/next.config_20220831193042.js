59a0cf04309087435d680d35dcd7e9728372de0d
1realjulius <iamjuliuseghan@gmail.com>
2022-08-31 19:34:58 -0700

    lg

---
+++ .history/next.config_20220831193042.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+    trailingSlash: true,
+    reactStrictMode: true,
+    assetPrefix: './',
+
+  },
+  reactStrictMode: true,
+}
